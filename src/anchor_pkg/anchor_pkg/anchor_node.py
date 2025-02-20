10192c746e357f693eeb75a4e39eb76ccf8c8d7f
Tristan McGinnis <tristanmcginnis26@gmail.com>
2025-02-20 14:34:35 -0600

    debug output on anchor

--- src/anchor_pkg/anchor_pkg/anchor_node.py
+++ src/anchor_pkg/anchor_pkg/anchor_node.py
@@ -108,6 +108,7 @@ class SerialRelay(Node):
             self.exit(1)
 
     def send_cmd(self, msg):
+        self.get_logger().info(f"Sending command to MCU: {msg}")
         self.ser.write(bytes(msg, "utf8"))
 
     @staticmethod
