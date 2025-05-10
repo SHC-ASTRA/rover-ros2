50409e21125ea6d49be17568ce7e1ab62238e534
Tristan McGinnis <tristanmcginnis26@gmail.com>
2025-05-10 15:00:43 -0500

    chain arm control commands to send as commad to MCU

--- src/arm_pkg/arm_pkg/arm_node.py
+++ src/arm_pkg/arm_pkg/arm_node.py
@@ -128,19 +128,19 @@ class SerialRelay(Node):
 
         #Send controls for arm
         command = "can_relay_tovic,arm,39," + str(axis0) + "," + str(axis1) + "," + str(axis2) + "," + str(axis3) + "\n"
-        self.send_cmd(command)
+        #self.send_cmd(command)
         
         #Send controls for end effector
-        command = "can_relay_tovic,digit,35," + str(msg.effector_roll) + "\n"
-        self.send_cmd(command)
+        command += "can_relay_tovic,digit,35," + str(msg.effector_roll) + "\n"
+        #self.send_cmd(command)
         
-        command = "can_relay_tovic,digit,36,0," + str(msg.effector_yaw) + "\n"
-        self.send_cmd(command)
+        command += "can_relay_tovic,digit,36,0," + str(msg.effector_yaw) + "\n"
+        #self.send_cmd(command)
 
-        command = "can_relay_tovic,digit,26," + str(msg.gripper) + "\n"
-        self.send_cmd(command)
+        command += "can_relay_tovic,digit,26," + str(msg.gripper) + "\n"
+        #self.send_cmd(command)
 
-        command = "can_relay_tovic,digit,28," + str(msg.laser) + "\n"
+        command += "can_relay_tovic,digit,28," + str(msg.laser) + "\n"
         self.send_cmd(command)
         
         
