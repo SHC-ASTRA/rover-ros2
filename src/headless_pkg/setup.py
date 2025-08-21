10757c8c713a810b19e043c7c7f7d6953608d0e1
David Sharpe <ds0196@uah.edu>
2025-08-21 16:31:59 -0500

    fix: names in ROS2 files were incorrect for headless_full, add new service files for NUC

--- src/headless_pkg/setup.py
+++ src/headless_pkg/setup.py
@@ -7,8 +7,7 @@ setup(
     version='0.0.0',
     packages=find_packages(exclude=['test']),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
     ],
     install_requires=['setuptools'],
@@ -19,7 +18,7 @@ setup(
     license='All Rights Reserved',
     entry_points={
         'console_scripts': [
-            "headless = headless_pkg.headless:main",
+            "headless_full = src.headless_node:main",
         ],
     },
 )
