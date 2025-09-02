9471992d3bdeaaa6baf8ea92cbfab5509a376eb9
David <ds0196@uah.edu>
2025-09-02 02:50:47 -0500

    feat: add controller support

--- src/astra_arm_moveit_config/launch/demo.launch.py
+++ src/astra_arm_moveit_config/launch/demo.launch.py
@@ -22,6 +22,11 @@ from moveit_configs_utils.launch_utils import (
     DeclareBooleanLaunchArg,
 )
 
+from launch_param_builder import ParameterBuilder
+from launch_ros.actions import ComposableNodeContainer
+from launch_ros.descriptions import ComposableNode
+
+
 def generate_launch_description():
     moveit_config = MoveItConfigsBuilder("ASTRA_Arm", package_name="astra_arm_moveit_config").to_moveit_configs()
     # return generate_demo_launch(moveit_config)
@@ -105,6 +110,68 @@ def generate_launch_description():
             ],
         )
     )
+
+    ## SERVO
+
+    # This sets the update rate and planning group name for the acceleration limiting filter.
+    acceleration_filter_update_period = {"update_period": 0.01}
+    planning_group_name = {"planning_group_name": "astra_arm"}
+
+    # Get parameters for the Servo node
+    servo_params = {
+        "moveit_servo": ParameterBuilder("astra_arm_moveit_config")
+        .yaml("config/astra_arm_simulated_config.yaml")
+        .to_dict()
+    }
+    ld.add_action(
+        ComposableNodeContainer(
+            name="moveit_servo_demo_container",
+            namespace="/",
+            package="rclcpp_components",
+            executable="component_container_mt",
+            composable_node_descriptions=[
+                # Example of launching Servo as a node component
+                # Assuming ROS2 intraprocess communications works well, this is a more efficient way.
+                # ComposableNode(
+                #     package="moveit_servo",
+                #     plugin="moveit_servo::ServoServer",
+                #     name="servo_server",
+                #     parameters=[
+                #         servo_params,
+                #         moveit_config.robot_description,
+                #         moveit_config.robot_description_semantic,
+                #     ],
+                # ),
+                ComposableNode(
+                    package="servo_arm_twist_pkg",
+                    plugin="servo_arm_twist_pkg::JoyToServoPub",
+                    name="controller_to_servo_twist_node",
+                ),
+                ComposableNode(
+                    package="joy",
+                    plugin="joy::Joy",
+                    name="joy_node",
+                ),
+            ],
+            output="screen",
+        )
+    )
+    # Launch a standalone Servo node.
+    # As opposed to a node component, this may be necessary (for example) if Servo is running on a different PC
+    ld.add_action(
+        Node(
+            package="moveit_servo",
+            executable="servo_node_main",
+            parameters=[
+                servo_params,
+                moveit_config.robot_description,
+                moveit_config.robot_description_semantic,
+                moveit_config.robot_description_kinematics,
+            ],
+            output="screen",
+        )
+    )
+
 
     ld.add_action(
         IncludeLaunchDescription(
