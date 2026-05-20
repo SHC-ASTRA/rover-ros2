360d4cb1b420b36fa799cd3eeef288212f14d665
David <ds0196@uah.edu>
2026-05-20 17:33:33 -0500

    fix(arm): rate limit /joint_commands

--- src/anchor_pkg/launch/rover.launch.py
+++ src/anchor_pkg/launch/rover.launch.py
@@ -90,7 +90,10 @@ def generate_launch_description():
             executable="arm",
             name="arm",
             output="both",
-            parameters=[{"launch_mode": "anchor"}],
+            parameters=[
+                {"launch_mode": "anchor"},
+                {"use_ros2_control": LaunchConfiguration("use_ros2_control")},
+            ],
             on_exit=Shutdown(),
         )
     )
@@ -114,14 +117,10 @@ def generate_launch_description():
             output="both",
             parameters=[
                 {"launch_mode": "anchor"},
-                {
-                    "use_ros2_control": LaunchConfiguration(
-                        "use_ros2_control", default=False
-                    )
-                },
+                {"use_ros2_control": LaunchConfiguration("use_ros2_control")},
                 {
                     "rover_platform_override": LaunchConfiguration(
-                        "rover_platform_override", default="auto"
+                        "rover_platform_override"
                     )
                 },
             ],
